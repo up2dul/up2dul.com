89417e2b8de1b02240dd419ddd86739288687fc7
abdul <malik301002@gmail.com>
2023-01-17 06:40:55 +0700

    feat(pages): add dark mode classes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+import { cn } from '@/lib/utils';
+
 const Document = () => (
   <Html lang='en' className='scroll-smooth'>
     <Head />
-    <body className='bg-geyser-50 text-bunker-900'>
+    <body
+      className={cn(
+        'bg-geyser-50 text-bunker-900 transition-colors',
+        'dark:bg-bunker-900 dark:text-geyser-50',
+      )}
+    >
       <Main />
       <NextScript />
     </body>
