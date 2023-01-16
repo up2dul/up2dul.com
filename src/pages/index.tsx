25396361abdbe9794129277efee59818004ca759
abdul <malik301002@gmail.com>
2023-01-16 19:50:46 +0700

    feat(pages): temporary content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,22 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Hello Next.js</title>
-        <meta name='description' content='Crafted with using Next.js' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+const Home: NextPage = () => (
+  <>
+    <Head>
+      <title>Hello Next.js</title>
+      <meta name='description' content='Crafted using Next.js' />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
 
-      <h1>Hello Next.js</h1>
-    </>
-  );
-};
+    <h1>Hello Next.js h1</h1>
+    <h2>Hello Next.js h2</h2>
+    <h3>Hello Next.js h3</h3>
+    <h4>Hello Next.js h4</h4>
+    <h5>Hello Next.js h5</h5>
+
+    <p>this is paragraph</p>
+  </>
+);
 
 export default Home;
