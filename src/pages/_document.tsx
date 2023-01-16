6e524a5adcccaf74996eb5e9f57da9b11a2f4007
abdul <malik301002@gmail.com>
2023-01-16 19:55:33 +0700

    feat(pages): create _document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document = () => (
+  <Html lang='en' className='scroll-smooth'>
+    <Head />
+    <body className='bg-geyser-50 text-bunker-900'>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
