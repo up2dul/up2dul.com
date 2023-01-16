e896d2238eda835bc833856e9382f30d15329565
abdul <malik301002@gmail.com>
2023-01-17 06:39:25 +0700

    feat: ThemeProvider and tailwind darkMode config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
+import { ThemeProvider } from 'next-themes';
 import type { AppType } from 'next/app';
 
 import Layout from '@/components/layout/Layout';
 import '@/styles/globals.css';
 
 const MyApp: AppType = ({ Component, pageProps }) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  <ThemeProvider defaultTheme='dark'>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </ThemeProvider>
 );
 
 export default MyApp;
