0311c4ce54ce1e121a23fec3b6c9d83a817cf53e
abdul <malik301002@gmail.com>
2023-01-17 08:56:04 +0700

    feat(utils): create getCurrentTime function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,4 +7,23 @@ function cn(...inputs: ClassValue[]) {
 }
 //#endregion cn
 
-export { cn };
+//#region getCurrentTime
+function padTo2Digits(num: number) {
+  return String(num).padStart(2, '0');
+}
+
+const days: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const d = new Date(
+  new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' }),
+);
+
+function getCurrentTime() {
+  const day = days[d.getDay()] as string;
+  const hoursMinutes =
+    padTo2Digits(d.getHours()) + ':' + padTo2Digits(d.getMinutes());
+
+  return `${day}, ${hoursMinutes}`;
+}
+//#endregion getCurrentTime
+
+export { cn, getCurrentTime };
