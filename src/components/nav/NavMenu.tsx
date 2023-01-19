2b04125df9bedec0475da4854e09e451f646705f
abdul <malik301002@gmail.com>
2023-01-19 10:37:55 +0700

    feat(comps): add border to Content

--- src/components/nav/NavMenu.tsx
+++ src/components/nav/NavMenu.tsx
@@ -24,7 +24,12 @@ const NavMenu = () => {
 
       <Dialog.Portal>
         <Dialog.Overlay className='fixed inset-0 z-20 flex items-center justify-center backdrop-blur-sm'>
-          <Dialog.Content className='rounded-md bg-geyser-100 px-8 pt-5 pb-6 outline-none dark:bg-bunker-800'>
+          <Dialog.Content
+            className={cn(
+              'rounded-md border-2 border-bunker-800 bg-geyser-50 px-8 pt-5 pb-6 outline-none',
+              'dark:border-geyser-100 dark:bg-bunker-900',
+            )}
+          >
             <ul className='flex flex-col gap-5'>
               {navMenuList.map(({ href, content }) => (
                 <NavLink key={href} to={href} isActive={pathname === href}>
