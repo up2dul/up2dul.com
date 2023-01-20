f6c74e850c5676fd13e326bb5b8db842ee33d704
abdul <malik301002@gmail.com>
2023-01-20 18:03:40 +0700

    feat(comps): implement motion to NavMenu content and MyTooltip

--- src/components/MyTooltip.tsx
+++ src/components/MyTooltip.tsx
@@ -1,5 +1,8 @@
 import * as Tooltip from '@radix-ui/react-tooltip';
+import { motion } from 'framer-motion';
 import type { PropsWithChildren } from 'react';
+
+import { tooltipVariants } from '@/lib/motion';
 
 type MyTooltipProps = PropsWithChildren<{
   content: string;
@@ -18,7 +21,14 @@ const MyTooltip = ({ content, children }: MyTooltipProps) => (
           side='bottom'
           className='z-20 rounded-md bg-geyser-100 px-3 py-2 shadow-md dark:bg-bunker-800'
         >
-          <span className='text-sm font-medium'>{content}</span>
+          <motion.span
+            variants={tooltipVariants}
+            initial='hidden'
+            animate='show'
+            className='text-sm font-medium'
+          >
+            {content}
+          </motion.span>
           <Tooltip.Arrow className='fill-geyser-100 dark:fill-bunker-800' />
         </Tooltip.Content>
       </Tooltip.Portal>
--- src/components/nav/NavMenu.tsx
+++ src/components/nav/NavMenu.tsx
@@ -1,9 +1,11 @@
 import { useRouter } from 'next/router';
 import * as Dialog from '@radix-ui/react-dialog';
+import { motion } from 'framer-motion';
 import { Equal } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 import { navMenuList } from '@/lib/misc';
+import { navMenuVariants } from '@/lib/motion';
 import NavLink from './NavLink';
 
 const NavMenu = () => {
@@ -24,20 +26,26 @@ const NavMenu = () => {
 
       <Dialog.Portal>
         <Dialog.Overlay className='fixed inset-0 z-20 flex items-center justify-center backdrop-blur-sm'>
-          <Dialog.Content
-            className={cn(
-              'rounded-md border-2 border-bunker-800 bg-geyser-50 px-8 pt-5 pb-6 outline-none',
-              'dark:border-geyser-100 dark:bg-bunker-900',
-            )}
+          <motion.div
+            variants={navMenuVariants}
+            initial='hidden'
+            animate='show'
           >
-            <ul className='flex flex-col gap-5'>
-              {navMenuList.map(({ href, content }) => (
-                <NavLink key={href} to={href} isActive={pathname === href}>
-                  {content}
-                </NavLink>
-              ))}
-            </ul>
-          </Dialog.Content>
+            <Dialog.Content
+              className={cn(
+                'rounded-md border-2 border-bunker-800 bg-geyser-50 px-8 pt-5 pb-6 outline-none',
+                'dark:border-geyser-100 dark:bg-bunker-900',
+              )}
+            >
+              <ul className='flex flex-col gap-5'>
+                {navMenuList.map(({ href, content }) => (
+                  <NavLink key={href} to={href} isActive={pathname === href}>
+                    {content}
+                  </NavLink>
+                ))}
+              </ul>
+            </Dialog.Content>
+          </motion.div>
         </Dialog.Overlay>
       </Dialog.Portal>
     </Dialog.Root>
