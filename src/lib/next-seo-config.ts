0c2c18cc964a30d9b1e76cd64a30fe961433e84e
abdul <malik301002@gmail.com>
2023-01-22 22:19:03 +0700

    feat: create basic default SEO props

---
+++ src/lib/next-seo-config.ts
@@ -0,0 +1,10 @@
+import type { DefaultSeoProps } from 'next-seo';
+
+const SEO: DefaultSeoProps = {
+  titleTemplate: "%s | Abdul Malik's site",
+  defaultTitle: 'Abdul Malik',
+  description:
+    'My personal website, a website where I showcase some my projects, skills, contacts, etc.',
+};
+
+export default SEO;
