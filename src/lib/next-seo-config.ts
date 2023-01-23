e08fbf1b11b0a36e4aa6d58e1b7b9b8454202f00
abdul <malik301002@gmail.com>
2023-01-23 21:36:48 +0700

    feat: add some new properties

--- src/lib/next-seo-config.ts
+++ src/lib/next-seo-config.ts
@@ -5,6 +5,21 @@ const SEO: DefaultSeoProps = {
   defaultTitle: 'Abdul Malik',
   description:
     'My personal website, a website where I showcase some my projects, skills, contacts, etc.',
+  themeColor: '#0070f3',
+  openGraph: {
+    type: 'website',
+    title: 'Abdul Malik',
+    description:
+      'My personal website, a website where I showcase some my projects, skills, contacts, etc.',
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_SITE_URL}/api/og`,
+        width: 1200,
+        height: 630,
+        type: 'image/jpeg',
+      },
+    ],
+  },
 };
 
 export default SEO;
