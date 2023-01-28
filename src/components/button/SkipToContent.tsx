8a2243ef65c65a7be3ef02b95d5e8cd86944a452
abdul <malik301002@gmail.com>
2023-01-28 08:44:17 +0700

    feat(comps): add font weight

--- src/components/button/SkipToContent.tsx
+++ src/components/button/SkipToContent.tsx
@@ -5,7 +5,7 @@ const SkipToContent = () => (
     href='#main-content'
     aria-label='Skip to content'
     className={cn(
-      'absolute -top-10 left-10 z-20 -translate-y-12 rounded-md bg-bunker-50 p-1',
+      'absolute -top-10 left-10 z-20 -translate-y-12 rounded-md bg-bunker-50 p-1 font-medium',
       'transform transition-transform duration-200',
       'focus:top-8 focus:translate-y-3',
       'dark:bg-bunker-900',
