6e472e870670cd6ba817da27aaacc749ae3c884e
abdul <malik301002@gmail.com>
2023-02-10 23:32:01 +0700

    feat(pages): temporary additions

--- src/pages/about-site.tsx
+++ src/pages/about-site.tsx
@@ -2,6 +2,7 @@ import { NextSeo } from 'next-seo';
 import { motion } from 'framer-motion';
 
 import { slideVariants } from '@/lib/motion';
+import SectionTitle from '@/components/SectionTitle';
 
 const AboutSite = () => (
   <>
@@ -24,6 +25,39 @@ const AboutSite = () => (
     <motion.h2 variants={slideVariants(0.2)} initial='hidden' animate='show'>
       🌐 About this <span className='text-gradient'>website</span>
     </motion.h2>
+
+    <div className='mt-10 flex flex-col gap-6 self-start text-left'>
+      <motion.section
+        variants={slideVariants(0.4)}
+        initial='hidden'
+        animate='show'
+      >
+        <SectionTitle id='reason'>
+          📄 <span className='text-gradient'>Reason</span> on making
+        </SectionTitle>
+        <ul className='mt-3'>
+          <li>As implementation to learning Next.js</li>
+          <li>A place to showcase my portfolios</li>
+        </ul>
+      </motion.section>
+
+      <motion.section
+        variants={slideVariants(0.6)}
+        initial='hidden'
+        animate='show'
+      >
+        <SectionTitle id='tech-stack'>
+          ⚛️ <span className='text-gradient'>Tech</span> stack
+        </SectionTitle>
+        <ul className='mt-3'>
+          <li>Next.js</li>
+          <li>TypeScript</li>
+          <li>Tailwind CSS</li>
+          <li>Radix UI</li>
+          <li>Contentlayer + MDX</li>
+        </ul>
+      </motion.section>
+    </div>
   </>
 );
 
