59fdfce6647c8e85a4c654618923ca6e385d9c3e
abdul <malik301002@gmail.com>
2023-02-24 22:23:58 +0700

    feat(comps): add `a` with `text-link` className to MDXComponents

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -4,6 +4,9 @@ const MDXComponents = {
   h3: ({ id, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
     <SectionTitle id={id as string} className='mt-6 mb-3' {...props} />
   ),
+  a: ({ ...props }: React.HTMLAttributes<HTMLAnchorElement>) => (
+    <a className='text-link' {...props} />
+  ),
 };
 
 export default MDXComponents;
