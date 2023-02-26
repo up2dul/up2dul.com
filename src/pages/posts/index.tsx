9462f62d96bdd18442e585c6b146d63da3345cc8
abdul <malik301002@gmail.com>
2023-02-26 21:38:22 +0700

    feat(pages): posts content

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,6 +1,18 @@
 import { NextSeo } from 'next-seo';
+import { motion } from 'framer-motion';
+import Balancer from 'react-wrap-balancer';
 
-const Posts = () => (
+import { allPosts, type Post } from 'contentlayer/generated';
+
+import Card from '@/components/Card';
+import { slideVariants } from '@/lib/motion';
+import { selectField } from '@/lib/utils';
+
+const Posts = ({
+  posts,
+}: {
+  posts: Pick<Post, 'slug' | 'title' | 'description' | 'tags'>[];
+}) => (
   <>
     <NextSeo
       title='Posts'
@@ -20,8 +32,49 @@ const Posts = () => (
       }}
     />
 
-    <h2>Posts</h2>
+    <motion.section
+      variants={slideVariants(0.2)}
+      initial='hidden'
+      animate='show'
+    >
+      <h2>✍️ Posts</h2>
+      <p className='mt-3'>
+        <Balancer>My thoughts, notes, blog, or any things.</Balancer>
+      </p>
+    </motion.section>
+
+    <motion.section
+      variants={slideVariants(0.4)}
+      initial='hidden'
+      animate='show'
+      className='mt-10 grid w-full grid-cols-1 gap-12 lg:grid-cols-2'
+    >
+      {posts.map((post, idx) => (
+        <Card
+          {...post}
+          key={post.slug}
+          href={`/posts/${post.slug}`}
+          order={`0${++idx}`}
+        />
+      ))}
+    </motion.section>
   </>
 );
+
+export function getStaticProps() {
+  const posts = allPosts
+    .map((post) =>
+      selectField(post, ['slug', 'title', 'description', 'tags', 'createdAt']),
+    )
+    .sort(
+      (a, b) => Number(new Date(b.createdAt)) - Number(new Date(a.createdAt)),
+    );
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 
 export default Posts;
